96fe71306edb67183758c981f29029c72590b65c
ovjust <56216004@qq.com>
2015-06-15 16:13:05 +0800

    add xpoLogWriter

---
+++ Ovjust.DbProvider/XpoLogWriterTextFile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Ovjust.DbXpoProvider
+{
+    public class XpoLogWriterTextFile : TraceListener
+    {
+        string fileName = "log.txt";
+        FileStream fs;
+        public XpoLogWriterTextFile()
+        {
+             fs=  File.Open(fileName, FileMode.OpenOrCreate,FileAccess.ReadWrite);
+            File
+        }
+        public override void Write(string message)
+        {
+           fs.w
+        }
+        public override void WriteLine(string message)
+        {
+            outputWindow.AppendText(message + "\r\n");
+        }
+    }
+}
